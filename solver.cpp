5d5c247369aed842dec89afdbad4025c69556fd8
NaomiO <44748207+NaomiO@users.noreply.github.com>
2020-04-26 10:21:15 +0300

    first commit

---
+++ solver.cpp
@@ -0,0 +1,14 @@
+#include "solver.hpp"
+using namespace std;
+using namespace solver;
+
+complex<double> solve(ComplexVariable y){
+    complex<double> comp;
+    return comp;
+}
+
+
+
+double solve(RealVariable x){
+    return 0;
+}
