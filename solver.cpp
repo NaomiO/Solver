967bcb3b51946a9e6ba19f028b680809075bc9e2
NaomiO <44748207+NaomiO@users.noreply.github.com>
2020-04-29 16:29:57 +0300

    commit for 3.1

--- solver.cpp
+++ solver.cpp
@@ -1,14 +1,90 @@
+
 #include "solver.hpp"
-using namespace std;
-using namespace solver;
 
-complex<double> solve(ComplexVariable y){
-    complex<double> comp;
-    return comp;
-}
+using solver::RealVariable, solver::ComplexVariable;
 
 
+//Constructors for equations with real variables
+RealVariable::RealVariable():num(0){}
+RealVariable::RealVariable(double num):num(num){}
 
-double solve(RealVariable x){
-    return 0;
-}
+//Constructors for equations with complex variables
+ComplexVariable::ComplexVariable() : real(0),imag(0){}
+ComplexVariable::ComplexVariable(double r1,double im1) : real(r1),imag(im1){}
+
+/**Operators for equations with real variables**/
+
+//Operator+
+RealVariable& solver::operator+(const double number, RealVariable& x){    return x;}
+RealVariable& solver::operator+(RealVariable& x, const double number){    return x;}
+RealVariable& solver::operator+(RealVariable& x1, RealVariable& x2){    return x1;}
+
+//Operator-
+RealVariable& solver::operator-(const double number, RealVariable& x){    return x;}
+RealVariable& solver::operator-(RealVariable& x, const double number){    return x;}
+RealVariable& solver::operator-(RealVariable& x1, RealVariable& x2){    return x1;}
+
+//Operator*
+RealVariable& solver::operator*(const double number, RealVariable& x){    return x;}
+RealVariable& solver::operator*(RealVariable& x, const double number){    return x;}
+RealVariable& solver::operator*(RealVariable& x1, RealVariable& x2){    return x1;}
+
+//Operator/
+RealVariable& solver::operator/(const double number, RealVariable& x){    return x;}
+RealVariable& solver::operator/(RealVariable& x, const double number){    return x;}
+RealVariable& solver::operator/(RealVariable& x1, RealVariable& x2){    return x1;}
+
+//Operator^
+RealVariable& solver::operator^(RealVariable& x, const double number){    return x;}
+
+//Operator==
+RealVariable& solver::operator==(const double number, RealVariable& x){    return x;}
+RealVariable& solver::operator==(RealVariable& x, const double number){    return x;}
+RealVariable& solver::operator==(RealVariable& x1, RealVariable& x2){   return x1;}
+
+
+/**Operators for equations with complex variables**/
+
+//Operator+
+ComplexVariable& solver::operator+(const double number,ComplexVariable& y){    return y;}
+ComplexVariable& solver::operator+(ComplexVariable& y, const double number){    return y;}
+ComplexVariable& solver::operator+(ComplexVariable& y1, ComplexVariable& y2){    return y1;}
+ComplexVariable& solver::operator+(complex<double> number,ComplexVariable& y){    return y;}
+ComplexVariable& solver::operator+(ComplexVariable& y ,complex<double> number){    return y;}
+
+//Operator-
+ComplexVariable& solver::operator-(const double number,ComplexVariable& y){    return y;}
+ComplexVariable& solver::operator-(ComplexVariable& y, const double number){    return y;}
+ComplexVariable& solver::operator-(ComplexVariable& y1, ComplexVariable& y2){    return y1;}
+ComplexVariable& solver::operator-(complex<double> number,ComplexVariable& y){    return y;}
+ComplexVariable& solver::operator-(ComplexVariable& y ,complex<double> number){    return y;}
+
+//Operator*
+ComplexVariable& solver::operator*(const double number, ComplexVariable& y){    return y;}
+ComplexVariable& solver::operator*(ComplexVariable& y, const double number){    return y;}
+ComplexVariable& solver::operator*(ComplexVariable& y1, ComplexVariable& y2){    return y1;}
+ComplexVariable& solver::operator*(complex<double> number, ComplexVariable& y){    return y;}
+ComplexVariable& solver::operator*(ComplexVariable& y ,complex<double> number){    return y;}
+
+//Operator/
+ComplexVariable& solver::operator/(const double number,ComplexVariable& y){    return y;}
+ComplexVariable& solver::operator/(ComplexVariable& y, const double number){    return y;}
+ComplexVariable& solver::operator/(ComplexVariable& y1, ComplexVariable& y2){    return y1;}
+ComplexVariable& solver::operator/(complex<double> number,ComplexVariable& y){    return y;}
+ComplexVariable& solver::operator/(ComplexVariable& y ,complex<double> number){    return y;}
+
+//Operator^
+ComplexVariable& solver::operator^(ComplexVariable& y, const double number){    return y;}
+
+//Operator==
+ComplexVariable& solver::operator==(const double number,ComplexVariable& y){    return y;}
+ComplexVariable& solver::operator==(ComplexVariable& y, const double number){    return y;}
+ComplexVariable& solver::operator==(ComplexVariable& y1, ComplexVariable& y2){    return y1;}
+ComplexVariable& solver::operator==(complex<double> number,ComplexVariable& y){    return y;}
+ComplexVariable& solver::operator==(ComplexVariable& y ,complex<double> number){    return y;}
+
+
+/*******Solve functions******/
+
+double solver::solve(RealVariable &exercise){    return 0.0;}
+std::complex<double> solver::solve(ComplexVariable &exercise){    return 0.0;}
--- solver.hpp
+++ solver.hpp
@@ -1,21 +1,105 @@
-
-#include <complex> 
+#pragma once
+#include <iostream>
+#include <complex>
 using namespace std;
 
-namespace solver{
+namespace solver
+{
+    /********Class for REAL VARIABLES in equations for example x^2 + 3*x - 4*********/
 
-class RealVariable{
-double realVar;
+    class RealVariable
+    {
+        double num; 
 
-};
+    public:
+        RealVariable();             
+        RealVariable(double num);
 
-class ComplexVariable{
-complex<double> compVar;
+        //Operator+ for addition
+        friend RealVariable& operator+(const double num, RealVariable& x); 
+        friend RealVariable& operator+(RealVariable& x, const double num);
+        friend RealVariable& operator+(RealVariable& x1, RealVariable& x2); 
+
+        //Operator- for substraction
+        friend RealVariable& operator-(const double num, RealVariable& x); 
+        friend RealVariable& operator-(RealVariable& x, const double num); 
+        friend RealVariable& operator-(RealVariable& x1, RealVariable& x2); 
+
+        //Operator* for multiplication
+        friend RealVariable& operator*(const double num, RealVariable& x); 
+        friend RealVariable& operator*(RealVariable& x, const double num); 
+        friend RealVariable& operator*(RealVariable& x1, RealVariable& x2); 
+
+        //Operator/ for division       
+        friend RealVariable& operator/(const double num, RealVariable& x);
+        friend RealVariable& operator/(RealVariable& x, const double num); 
+        friend RealVariable& operator/(RealVariable& x1, RealVariable& x2); 
+
+        //Operator^ for adding power to x
+        friend RealVariable& operator^(RealVariable& x, const double num); 
+
+        //Operator== for equality verification
+        friend RealVariable& operator==(const double num, RealVariable& x); 
+        friend RealVariable& operator==(RealVariable& x, const double num); 
+        friend RealVariable& operator==(RealVariable& x1, RealVariable& x2);
+
+    }; 
 
 
-};
+    /*********Class for COMPLEX VARIABLES in equations for example 2*y+3i*********/
 
-double solve(RealVariable x);
-complex<double> solve(ComplexVariable y);
+    class ComplexVariable
+    {
+        double real,imag;
 
-}
+    public:
+        ComplexVariable();
+        ComplexVariable(double real,double imag);
+        
+        //Operator+ for addition
+        friend ComplexVariable& operator+(const double num, ComplexVariable& y); 
+        friend ComplexVariable& operator+(ComplexVariable& y, const double num); 
+        friend ComplexVariable& operator+(ComplexVariable& y1, ComplexVariable& y2); 
+        friend ComplexVariable& operator+(complex<double> num, ComplexVariable& y); 
+        friend ComplexVariable& operator+(ComplexVariable& y, complex<double> num); 
+
+        //Operator- for substraction
+        friend ComplexVariable& operator-(const double num, ComplexVariable& y); 
+        friend ComplexVariable& operator-(ComplexVariable& y, const double num);
+        friend ComplexVariable& operator-(ComplexVariable& y1, ComplexVariable& y2); 
+        friend ComplexVariable& operator-(complex<double> num, ComplexVariable& y); 
+        friend ComplexVariable& operator-(ComplexVariable& y, complex<double> num); 
+
+        //Operator* for multiplication
+        friend ComplexVariable& operator*(const double num, ComplexVariable& y); 
+        friend ComplexVariable& operator*(ComplexVariable& y, const double num); 
+        friend ComplexVariable& operator*(ComplexVariable& y1, ComplexVariable& y2); 
+        friend ComplexVariable& operator*(complex<double> num, ComplexVariable& y); 
+        friend ComplexVariable& operator*(ComplexVariable& y, complex<double> num); 
+
+        //Operator/ for division
+        friend ComplexVariable& operator/(const double num, ComplexVariable& y); 
+        friend ComplexVariable& operator/(ComplexVariable& y, const double num); 
+        friend ComplexVariable& operator/(ComplexVariable& y1, ComplexVariable& y2);
+        friend ComplexVariable& operator/(complex<double> num, ComplexVariable& y); 
+        friend ComplexVariable& operator/(ComplexVariable& y, complex<double> num); 
+
+        //Operator^ for adding a power to x
+        friend ComplexVariable& operator^(ComplexVariable& y, const double num); 
+
+        //Operator== for equality verification
+        friend ComplexVariable& operator==(const double num, ComplexVariable& y); 
+        friend ComplexVariable& operator==(ComplexVariable& y, const double num); 
+        friend ComplexVariable& operator==(ComplexVariable& y1, ComplexVariable& y2); 
+        friend ComplexVariable& operator==(complex<double> num, ComplexVariable& y); 
+        friend ComplexVariable& operator==(ComplexVariable& y, complex<double> num); 
+
+    }; 
+
+    //Function to solve equations with real variables
+    double solve(RealVariable &exercise);
+    
+    //Function to solve equations with complex variables
+    std::complex<double> solve(ComplexVariable &exercise);
+
+}; 
