7d17f47c20ab15d5df0ce5757799d350451fb729
NaomiO <44748207+NaomiO@users.noreply.github.com>
2020-04-30 15:44:42 +0300

    last commiy 3.1

--- Test.cpp
+++ Test.cpp
@@ -7,7 +7,11 @@ using namespace std;
 
 TEST_CASE("Test for real variables"){
   RealVariable x;
-
+    CHECK((solve(x == 0)) == 0);
+    CHECK((solve(2*x == 0)) == 0);
+    CHECK((solve(-2*x == 0)) == 0);
+    CHECK((solve((0*(x^2)) + 2*x == 2) == 1));
+    CHECK((solve(0*(x^2) + 2*x == 5*x) == 0));
     CHECK((solve(x == 1)) == 1);
     CHECK((solve(x == -1)) == -1);
     CHECK((solve(2*x == 10)) == 5);
@@ -31,11 +35,60 @@ TEST_CASE("Test for real variables"){
     CHECK((solve((x^2) + 2*x == 3*x + 2) == -1));
     CHECK( (solve((x^2) + 2*x + 4.0 == 20 + 6.0*x/2 - x) == 4));
     CHECK((solve((x^2) + 2*x + 4.0 == 20 + 6.0*x/2 - x) == -4));
-
+    CHECK((solve(-5*x-4 == 11)) == -3);
+    CHECK(solve(2*x == 6) == 3);
+    CHECK(solve(3*x - 1 == 20) == 7);
+    CHECK(solve(x*2 -6 == 30) == 18);
+    CHECK(solve(2*x == 10) == 5);
+    CHECK(solve(3*x -10 -1 == 10) == 7);
+    CHECK(solve((2+3)*x-5 == 10) == 3);
+    CHECK(solve(2*x -5 == 10+1) == 8 );
+    CHECK(solve(2*x == 8*2) == 8);
+    CHECK(solve(2*x-4 == 14-4) == 7);
+    CHECK(solve(x+x-4 == 10) == 7);
+    CHECK(solve(3*x-x-4 == 10) == 7);
+    CHECK(solve(4*x-2*x-4 == 10) == 7);
+    CHECK(solve(2*x-4 == 10) == 7);
+    CHECK(solve(1*x + 2*x - x -4 == 10) == 7);
+    CHECK(solve(10 == 2*x - 4) == 7);
+    CHECK(solve(3*x -4 == 10+x) == 7);
+    CHECK(solve(2*x/2-4 == 10-x) == 7);
+    CHECK(solve((2 + 2*x)/2 -5 == 10) == 14);
+    CHECK(solve(4*x-2 == 10 + 2*x -2) == 5);
+    CHECK(solve(2*x-4.0 == 10.0) == 7);
+    CHECK(solve(2.0*x-4.0 == 10) == 7);
+    CHECK(solve(-2*x-4.0 == 10.0) == -7);
+    CHECK(solve(x == 1) == 1);
+    CHECK(solve(x+3 == 3) == 0);
+    CHECK(solve(x-4.0 == 10.0) == 14);
+    CHECK(solve(x+x == 2*x) == 0);
+    CHECK(solve(2*x - x == 2) == 2);
+    CHECK(solve(3*x - 2*x == 0) == 0);
+    CHECK(solve(2*x == x*3) == 0);
+    
     CHECK_THROWS( solve( (x^2) == -1) );
     CHECK_THROWS( solve( (2*(x^2)) == -1) );
     CHECK_THROWS( solve( (x^2)/2 == -1) );
     CHECK_THROWS( solve( (x^2) + 2*x == -1) );
+    CHECK_THROWS(solve( 0*(x^2) == -1));
+    CHECK_THROWS( solve( (x^2)/0 == 0) );
+    CHECK_THROWS(solve((2*x^2)/0 == 16));
+    CHECK_THROWS(solve((2*x^2)/(x-x) == 16));
+    CHECK_THROWS(solve((2*x^2)*x == 0));
+    CHECK_THROWS(solve((2*x^1)/x == 0));
+    CHECK_THROWS(solve((x^3) == 0));
+    CHECK_THROWS(solve((3*x^2)*x == 0));
+    CHECK_THROWS(solve((x^-1) == 0));
+    CHECK_THROWS(solve((x^2)*(x^2) == 0));
+    CHECK_THROWS(solve(2*x-2*x == 2));
+    CHECK_THROWS(solve(2*x/2*x == 2));
+    CHECK_THROWS(solve(2*x/(2*x-1) == 1));
+    CHECK_THROWS(solve(x-x == 2));
+    CHECK_THROWS(solve(3*x + 2 == 3 + 3*x));
+    CHECK_THROWS(solve(3*x/0 == 2));
+    CHECK_THROWS(solve(3*x/(2-2) == 2));
+    CHECK_THROWS(solve((2*x+1)*(x^2) == 2));
+
 
     }
 
